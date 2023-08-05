adbd67ccecdce8869c5076bf76918ef53814c087
Pushpender Rajput <pushpenderrajputsp@gmail.com>
2023-08-05 17:28:41 +0530

    added emailfield

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date, datetime
